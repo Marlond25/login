8313c62e63c762e46afa70cdbcc2abbb7d0d2c4a
Marlon ryzen <enginer26mj@gmail.com>
2020-08-07 13:33:44 -0500

    Fetch to hash API is working

--- login-vanilla-js/index.js
+++ login-vanilla-js/index.js
@@ -1,10 +1,35 @@
-import db from "./modules/mck-db.js";
+// import db from "./modules/mck-db.js";
 
-document.getElementByName("send").addEventListenner("click", search);
+function search(event) {
+  event.preventDefault();
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const url = "http://127.0.0.1:5000/hash";
+  let data = new FormData();
+  data.append("password", password);
 
-const search = () => {
-  preventDefault();
-  const username = document.getElementByName("username").value;
-  const password = document.getElementByName("password").value;
-  return console.log(db(username, password));
+  // const request = new Request(url, {
+  //     method: "POST",
+  //     // mode: "no-cors",
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     },
+  //     body: data
+  // });
+  // const response = await fetch(request);
+  // const text = await response.json();
+  // console.log(text.password);
+
+var requestOptions = {
+  method: "POST",
+  body: data,
+  redirect: "follow"
+};
+
+fetch(url, requestOptions)
+  .then(response => response.json())
+  .then(result => console.log(result.password))
+  .catch(error => console.log("error", error));
 }
+
+document.getElementById("send").addEventListener("click", search);
